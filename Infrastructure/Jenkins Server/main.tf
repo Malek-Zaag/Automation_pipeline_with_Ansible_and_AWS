7cfce70f9fc67034b04ff4407d9d638b304aeac9
Malek <zaag.malek1@gmail.com>
2023-11-07 21:35:33 +0100

    deployed-jenkins-server

--- Infrastructure/Jenkins Server/main.tf
+++ Infrastructure/Jenkins Server/main.tf
@@ -39,6 +39,10 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
   allocation_method   = "Dynamic"
 }
 
+output "public-ip" {
+  value = azurerm_public_ip.my_terraform_public_ip.ip_address
+}
+
 
 resource "azurerm_network_security_group" "my_terraform_nsg" {
   name                = "myNetworkSecurityGroup"
@@ -58,7 +62,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
   }
   security_rule {
     name                       = "SCP"
-    priority                   = 1001
+    priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -69,7 +73,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
   }
   security_rule {
     name                       = "Web"
-    priority                   = 1001
+    priority                   = 1003
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -80,12 +84,12 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
   }
   security_rule {
     name                       = "Jenkins"
-    priority                   = 1001
+    priority                   = 1004
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "5000"
+    destination_port_range     = "8080"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
@@ -117,11 +121,13 @@ resource "azurerm_virtual_machine" "jenkins_server" {
   vm_size               = "Standard_DS1_v2"
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
 
+
   storage_os_disk {
     name              = "myOsDisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+    managed_disk_type = "Premium_LRS"
+    disk_size_gb      = "64"
   }
 
   storage_image_reference {
@@ -139,11 +145,24 @@ resource "azurerm_virtual_machine" "jenkins_server" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
-
+  connection {
+    type     = "ssh"
+    user     = var.admin_username
+    password = var.admin_password
+    host     = azurerm_public_ip.my_terraform_public_ip.ip_address
+  }
   provisioner "remote-exec" {
     inline = ["sudo apt update",
       "sudo apt install python3 ",
       "sudo apt install ansible",
+      "sudo apt install openjdk-11-jdk -y",
+      "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
+      "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
+      "sudo apt update",
+      "sudo apt install jenkins -y",
+      "sudo systemctl enable --now jenkins",
+      "sudo ufw allow 8080",
+      "sudo ufw enable",
     ]
   }
 }
