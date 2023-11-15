09c2a481116129281b987ff3e976d19c46d8f068
Malek <zaag.malek1@gmail.com>
2023-11-15 17:46:54 +0100

    updating_vm_size

--- Infrastructure/Jenkins Server/main.tf
+++ Infrastructure/Jenkins Server/main.tf
@@ -118,7 +118,7 @@ resource "azurerm_virtual_machine" "jenkins_server" {
   name                  = "jenkins_server"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = "Standard_D2s_v3"
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
 
 
